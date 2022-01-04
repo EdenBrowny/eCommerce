6502039ad0eb8a697714bde9eca622598b3b6ee3
TheGreatMan <orisakwepeter@gmail.com>
2022-01-04 10:25:49 +0100

    first commit

---
+++ javascriptcrashcourse/crashcourse.js
@@ -0,0 +1,24 @@
+console.log("hello")
+//alert("hello this is peter")
+
+// Variables
+
+var b = "soomthie";
+console.log(b)
+var someNumber = 45
+alert('someNumber');
+
+
+
+var age = prompt('what is your number:')
+
+document.getElementById('SomeText').innerHTML = age;
+
+
+// numbers in javascript
+for (var i = 1; i < 101; i++) {
+    if (i % 15 == 0) console.log("FizzBuzz");
+    else if (i % 3 == 0) console.log("Fizz");
+    else if (i % 5 == 0) console.log("Buzz");
+    else console.log(i);
+}
